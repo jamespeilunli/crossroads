86c6c550325401fdee6340257aa4629ff8930101
James Li <jamespeilunli@gmail.com>
2024-09-08 16:29:07 -0700

    remove useless imports

--- cli/main.py
+++ cli/main.py
@@ -1,11 +1,6 @@
-import os
-from dotenv import load_dotenv
-import openai
 from lib.crossroads import Crossroads
-
 from cli.cli import CLI
 
 crossroads = Crossroads()
 cli = CLI(crossroads)
 cli.run()
-
