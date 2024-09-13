375385db33b93addbbc7f04446c89bcaafe068de
James Li <jamespeilunli@gmail.com>
2024-09-12 18:25:39 -0700

    flask boilerplate

---
+++ web/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/add')
+def data():
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+
+    response = jsonify({
+        "received": {
+            "a": a,
+            "b": b,
+        },
+        "message": f"{a} + {b} = {a + b}",
+        "result": a + b,
+    })
+    response.status_code = 200
+
+    return response
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
